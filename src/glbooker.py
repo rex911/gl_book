39f5c800a1900f55a0b8ac7f078ae4927b3fe994
rex911 <rexliu01@gmail.com>
2020-09-29 09:07:16 -0400

    reflect goodlife time slot changes

--- src/glbooker.py
+++ src/glbooker.py
@@ -11,8 +11,8 @@ class GLBooker(object):
                  password,
                  url='https://www.goodlifefitness.com/members/bookings/workout',
                  days_later=7,
-                 workout_start='6:00PM',
-                 workout_end='7:00PM'):
+                 workout_start='6:15PM',
+                 workout_end='7:15PM'):
         """Main class for the booker
 
         Args:
